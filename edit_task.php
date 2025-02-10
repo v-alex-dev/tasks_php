4ced800aafeb1a3d1bbcb4ef0f93b421deb8988f
v-alex-dev <vensalex1991@gmail.com>
2025-02-10 15:37:31 +0100

    Add the script to change the done value

---
+++ edit_task.php
@@ -0,0 +1,17 @@
+<?php
+$filename = __DIR__ . "/data/tasks.json";
+
+$_GET = filter_input_array(INPUT_GET, FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+$id = $_GET['id'] ?? '';
+if ($id) {
+    $data = file_get_contents($filename);
+    $task = json_decode($data, true);
+
+    if (count($task)) {
+        $taskIndex = array_search($id, array_column($task, 'id'));
+        $task[$taskIndex]['done'] = !$task[$taskIndex]['done'];
+        file_put_contents($filename, json_encode($task));
+    }
+}
+
+header('Location: http://localhost:3000');
